d928d6acc84faa8c90f5a25e9564468e84541afa
lucky7171 <kitonyi.muthoki22@students.dkut.ac.ke>
2023-07-24 17:02:38 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,12 +14,12 @@ sum = 0;
 srand(time(NULL));
 for (i = 0; i < 100; i++)
 {
-pass[i] = rand() % 78);
+pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
 if ((2772 - sum) - '0' < 780)
-{
-n = 2772 - sum -'0';
+}
+n = 2772 - sum - '0';
 sum += n;
 putchar(n + '0');
 break;
