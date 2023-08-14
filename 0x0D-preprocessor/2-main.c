a71baa9d7708176bdb8c1adbe688ea222294e724
lucky7171 <kitonyi.muthoki22@students.dkut.ac.ke>
2023-08-14 13:28:01 -0700

    njikik

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints the current working file
+* Return:0
+*
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(i) ((i) > 0 ? i : (-i))
+#endif
